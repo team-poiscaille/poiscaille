0217d5498c5282b20c471d8d7b108f1b2d18d9d3
Khinenw <deu07115@gmail.com>
2018-12-21 00:01:00 +0900

    Added editorconfig in order to adapt airbnb

--- app/pages/Game.vue
+++ app/pages/Game.vue
@@ -1,50 +1,53 @@
 <template>
-	<main class="Game">
-		<section class="ControlBar">
-			<minimap></minimap>
-			<unit-info></unit-info>
-			<unit-skills></unit-skills>
+  <main class="Game">
+    <section class="ControlBar">
+      <minimap></minimap>
+      <unit-skills></unit-skills>
 
-			<div class="NutrientsView">
+      <div class="NutrientsView">
 
-			</div>
-		</section>
+      </div>
+    </section>
 
-		<transition name="Dialog">
-			<factory v-if="selectedUnit.type === 'factory'"></factory>
-		</transition>
+    <transition name="Dialog">
+      <factory v-if="selectedUnit && selectedUnit.type === 'factory'"></factory>
+    </transition>
 
-		<transition name="Dialog">
-			<dna-list v-if="dnaOpen"></dna-list>
-		</transition>
-	</main>
+    <transition name="Dialog">
+      <dna-list v-if="dnaOpen"></dna-list>
+    </transition>
+
+    <transition name="Dialog">
+      <unit-info v-if="selectedUnit"></unit-info>
+    </transition>
+  </main>
 </template>
 
 <style lang="less" scoped>
 </style>
 
 <script>
-	import Minimap from "./Minimap";
-	import UnitInfo from "./UnitInfo";
-	import UnitSkills from "./UnitSkills";
+  import Minimap from "./Minimap";
+  import UnitInfo from "./UnitInfo";
+  import UnitSkills from "./UnitSkills";
 
-	export default {
-		components: {
-			Minimap,
-			UnitInfo,
-			UnitSkills
-		},
+  export default {
+    components: {
+      Minimap,
+      UnitInfo,
+      UnitSkills
+    },
 
-		data() {
-			return {
-				dnaOpen: false,
-			};
-		},
+    data() {
+      return {
+        dnaOpen: false,
+      };
+    },
 
-		computed: {
-			selectedUnit() {
-				return null;
-			}
-		}
-	};
+    computed: {
+      selectedUnit() {
+        return null;
+      }
+    }
+  };
 </script>
