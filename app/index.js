d5efd192139eff8623eb477220b0850a3e25c7ed
Khinenw <deu07115@gmail.com>
2018-12-21 18:40:32 +0900

    Lint fix

--- app/index.js
+++ app/index.js
@@ -1,21 +1,23 @@
-import makeStore from "./src/utils/makeStore";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
+import Poiscaille from './src/Poiscaille';
 
-import App from "./App.vue";
-import Poiscaille from "./src/Poiscaille";
-import Vue from "vue";
-import Vuex from "vuex";
+import makeStore from './src/utils/makeStore';
 
-import "./less/index.less";
+import './less/index.less';
 
 Vue.use(Vuex);
 
 const game = new Poiscaille();
 const store = makeStore(game);
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
   render(h) {
     return h(App);
-  }
+  },
 });
+/* eslint-enable no-new */
