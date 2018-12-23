0e0edf912701f0626e0b3114acb05aa8822bf433
Khinenw <deu07115@gmail.com>
2018-12-23 13:11:58 +0900

    Caching EntityCell colors

--- app/src/entity/EntityCell.js
+++ app/src/entity/EntityCell.js
@@ -1,27 +1,44 @@
 import Color from 'color';
 import Entity from './Entity';
 
+const colors = [
+  '#e91e63',
+  '#4caf50',
+  '#2196f3',
+];
+
+const colorGenerated = colors.map(color => {
+  const colorArray = Color(color).rgb().array();
+  return [1, 0.3, 0.2].map(opacity => `rgba(${[...colorArray, opacity].join(',')})`);
+});
+
 class EntityCell extends Entity {
   type = 'EntityCell';
+  static colors = colorGenerated;
+
+  static COLOR_R = 0;
+  static COLOR_G = 1;
+  static COLOR_B = 2;
 
   constructor(game, x, y) {
     super(game, x, y);
 
-    this.colorArray = [255, 255, 255];
     this.radius = 70;
+    this.color = EntityCell.COLOR_G;
   }
 
-  set color(string) {
-    this.colorArray = Color(string).rgb().array();
+  get colorList() {
+    return this.colors[this.color];
   }
 
   doRender(ctx, canvas, renderer) {
     const pos = renderer.getRenderPosition(this);
+    const colorList = this.colorList;
     const gradient = ctx.createRadialGradient(pos.x, pos.y, 0, pos.x, pos.y, this.radius);
-    gradient.addColorStop(1, `rgba(${[...this.colorArray, 0.3].join(',')})`);
-    gradient.addColorStop(0.9, `rgba(${[...this.colorArray, 0.2].join(',')})`);
-    gradient.addColorStop(0.75, `rgba(${[...this.colorArray, 1].join(',')})`);
-    gradient.addColorStop(0.1, `rgba(${[...this.colorArray, 0.3].join(',')})`);
+    gradient.addColorStop(1, colorList[1]);
+    gradient.addColorStop(0.9, colorList[2]);
+    gradient.addColorStop(0.75, colorList[0]);
+    gradient.addColorStop(0.1, colorList[1]);
 
     ctx.fillStyle = gradient;
     ctx.beginPath();
